c81a7146886082a290b5a15ea8aecb04e613ad37
zhangqi <zhang.qi@trs.com.cn>
2025-05-07 16:02:23 +0800

    222

--- app/src/main/java/org/autojs/autojs/ui/main/MainActivity.java
+++
@@ -1,9 +0,0 @@
-@Override
-protected void onNewIntent(Intent intent) {
-    super.onNewIntent(intent);
-    Uri uri = intent.getData();
-    if (uri != null && "autojs6".equals(uri.getScheme())) {
-        // 处理scheme跳转，唤醒app
-        setIntent(intent);
-    }
-} 
